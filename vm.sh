a76258125a3c3bd18c410fd787818b04a01cb37c
Bibhu <bibhup_mishra@yahoo.com>
2021-04-01 05:41:54 -0500

    restructured

---
+++ vm.sh
@@ -0,0 +1,7 @@
+ssh -i mylinuxvms.pem azureuser@devopsmasterlinuxvm.centralus.cloudapp.azure.com
+
+ssh -i mylinuxvms.pem azureuser@devopsnode1linuxvm.centralus.cloudapp.azure.com
+
+ssh -i mylinuxvms.pem azureuser@devopsnode2linuxvm.centralus.cloudapp.azure.com
+
+ssh -i mylinuxvms.pem azureuser@devopsk8masterlinuxvm.centralus.cloudapp.azure.com
